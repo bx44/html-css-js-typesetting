e74826a104f1f05a56323b23613da0f282cc5fd1
Barak <barakbenhamu@gmail.com>
2019-12-30 18:49:01 -0500

    Finished commentary

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 pageCounter = 0;
 pageLimit = 10;
-startMasechet = "Bava_Batra";
+startMasechet = "Berakhot";
 startDaf = "2a";
 
 pageWidth = 561;
 pageHeight = 793;
 pagePadding = 20;
+
+commentarist = "Rashi";
 
 currentRef = startMasechet+'.'+startDaf;
 console.log(currentRef);
@@ -41,23 +43,28 @@ function addData(data){
     //console.log(data);
     // for commentary
     sectionCounter = 0;
-    data.he.forEach(element => {
+    finished = 0;
+    for(sectionCounter = 0; sectionCounter < data.he.length;){
+        element = data.he[sectionCounter];
+
         var sectionRef = currentRef+'.'+sectionCounter;
         sectionRef = sectionRef.replace(' ', '_');
         var newEl = '<span ref="'+sectionRef+'">'+element.trim() + ' </span>';
         $('.page[page="'+pageCounter+'"] .mainText').append(newEl);
+        
         if(isOverflowed()){
             $('.page[page="'+pageCounter+'"] .mainText span:last-child').remove();
             if(pageCounter < pageLimit){
                 addPage();
-                $('.page[page="'+pageCounter+'"] .mainText').append(newEl);
+                continue;
             } else {
-                return;
+                finished = 1;
+                break;
             }
         }
-        
+
         var commentary = data.commentary.filter(el => {
-            return el.collectiveTitle.en == "Rashi" && el.anchorVerse == sectionCounter;
+            return el.collectiveTitle.en == commentarist && el.anchorVerse == sectionCounter;
         });
         if (commentary.length > 0) {
             // console.log(commentary);
@@ -65,39 +72,43 @@ function addData(data){
             $('.page[page="'+pageCounter+'"] .commentary').append(newEl);
         }
         commentary.forEach(el => {
-            var comment = el.he.split('-', 2);
+            var comment = el.he.split(/[–-]/, 2);
+            console.log(comment);
             if (comment.length > 1) {
-                newEl = '<span ref="'+el.ref+'"><span class="commentAnchor">' + comment[0].trim() + '. </span>' + comment[1].trim() + ' </span>';
+                newEl = '<span ref="'+el.ref+'"><span class="commentAnchor">' + comment[0].trim() + '. </span> ' + comment[1].trim() + ' </span>';
             } else {
-                newEl = '<span ref="' + el.ref + '" class="noAnchor">' + comment[0].trim() + '</span>';
+                newEl = '<span ref="' + el.ref + '" class="noAnchor">' + comment[0].trim() + ' </span>';
             }
             $('div[anchorRef="'+sectionRef.split('.').join('\\.')+'"]').append(newEl);
         });
+
         if(isOverflowed()){
             var lastComment = $('.page[page="'+pageCounter+'"] .commentary div:last-of-type');
             var lastMain = $('.page[page="'+pageCounter+'"] .mainText span:last-of-type');
-            lastComment.detach();
-            lastMain.detach();
+            lastComment.remove();
+            lastMain.remove();
             if(pageCounter < pageLimit){
                 addPage();
-                lastMain.appendTo('.page[page="'+pageCounter+'"] .mainText');
-                lastComment.appendTo('.page[page="'+pageCounter+'"] .commentary');
-                if(isOverflowed()) {
-                    // // $('.page[page="'+pageCounter+'"]').css('overflow', 'hidden');
-                    $('.page[page="'+pageCounter+'"]').append('<div class="watermark">Overflowed</div>');
-                }
+                continue;
             } else {
-                return;
+                finished = 0;
+                break;
             }
         }
+
+        adjustFloats();
+
         sectionCounter++;
-    });
-    if (sectionCounter == data.he.length && !isOverflowed()) {
-        var next = data.next.substring(0, data.next.lastIndexOf(' ')).replace(' ', '_')+'.'+data.next.substring(data.next.lastIndexOf(' ') + 1);
-        currentRef = next;
-        // console.log(currentRef);
-        getData();
     }
+
+    if (finished == 1 || data.next == null) {
+        centerEndofChapter();
+        return;
+    }
+    var next = data.next.substring(0, data.next.lastIndexOf(' ')).replace(' ', '_')+'.'+data.next.substring(data.next.lastIndexOf(' ') + 1);
+    currentRef = next;
+        // console.log(currentRef);
+    getData();
 }
 
 function addPage(){
@@ -112,4 +123,35 @@ function getUrl(ref){
 
 function isOverflowed(page=pageCounter){
     return $('.page[page="'+page+'"] .commentary').outerHeight(true) >= (pageHeight - (pagePadding * 2)) || $('.page[page="'+pageCounter+'"] .mainText').outerHeight(true) >= (pageHeight - (pagePadding * 2));
+}
+
+function adjustFloats(page=pageCounter) {
+
+
+    mainText = $('.page[page="'+page+'"] .mainText');
+    commentary = $('.page[page="'+page+'"] .commentary');
+
+    if(mainText.outerHeight(true) > commentary.outerHeight(true)) {
+        if(!mainText.hasClass('lessCommentary')) mainText.addClass('lessCommentary');
+        if(!commentary.hasClass('lessCommentary')) commentary.addClass('lessCommentary');
+
+        if($('.page[page="'+page+'"] .mainText')[0].nextSibling == $('.page[page="'+page+'"] .commentary')[0]) {
+            mainText.detach();
+            mainText.appendTo($('.page[page="'+page+'"]'));
+        }
+    }
+
+    if(mainText.outerHeight(true) < commentary.outerHeight(true)) {
+        if(mainText.hasClass('lessCommentary')) mainText.removeClass('lessCommentary');
+        if(commentary.hasClass('lessCommentary')) commentary.removeClass('lessCommentary');
+        
+        if($('.page[page="'+page+'"] .commentary')[0].nextSibling == $('.page[page="'+page+'"] .mainText')[0]) {
+            commentary.detach();
+            commentary.appendTo($('.page[page="'+page+'"]'));
+        }
+    }    
+}
+
+function centerEndofChapter() {
+    $('span:contains("הדרן")').addClass('endOfChapter');
 }
